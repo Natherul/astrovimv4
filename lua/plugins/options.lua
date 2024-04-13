e898f2f15b0245e78381dc8612bfebcd3d73ca59
Natherul <natherul0@gmail.com>
2024-04-13 20:07:01 +0200

    convert old config to new worse v4 config

---
+++ lua/plugins/options.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@param opts AstroCoreOpts
+    opts = {
+      options = {
+        opt = {
+          relativenumber = false, -- sets vim.opt.relativenumber
+          number = true, -- sets vim.opt.number
+          spell = false, -- sets vim.opt.spell
+          signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+          wrap = false, -- sets vim.opt.wrap
+          autochdir = false,
+          tabstop = 4,
+          softtabstop = 4,
+          shiftwidth = 4,
+          -- Save undos longer so if we close vim its saved
+          swapfile = false,
+          backup = false,
+          undodir = os.getenv("HOME") .. "/.vim/undodir",
+          undofile = true,
+          -- Faster update speed, default i 4000
+          updatetime = 50,
+          -- Allow for seeing if search is hitting anything
+          hlsearch = false,
+          incsearch = true,
+        },
+      },
+    },
+  },
+}
